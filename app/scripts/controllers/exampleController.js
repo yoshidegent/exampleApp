3f5d4dd6111e7a0cb23bb128db24a6a5ff35461c
YDEAX41 <yoshi_degent@hotmail.com>
2015-09-30 12:28:07 +0200

    some extra example functionality

--- app/scripts/controllers/exampleController.js
+++ app/scripts/controllers/exampleController.js
@@ -4,9 +4,18 @@
   function ExampleController($scope) {
     console.log('create ExampleController');
 
+    $scope.data = [
+      {prop: 'a'}, {prop: 'b'}, {prop: 'c'}];
+
+
     $scope.$on('userclicked', function(event, data){
       console.log(data);
       $scope.message = data.number;
     });
+
+    $scope.submitForm = function(inputText)
+    {
+      console.log("submit " + inputText);
+    };
   }
 })();
