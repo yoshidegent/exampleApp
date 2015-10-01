e55ac714bb85eedeca892e50adebeb18c59d2090
YDEAX41 <yoshi_degent@hotmail.com>
2015-10-01 15:52:42 +0200

    added another directive combined with a view

---
+++ app/scripts/directives/customDirective.js
@@ -0,0 +1,10 @@
+(function () {
+  angular.module('exampleApp.directives')
+    .directive('customDirective', function () {
+
+      return {
+        templateUrl: 'views/customDirective.html'
+      }
+    })
+
+})();
