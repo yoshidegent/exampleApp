693c6603ee3ffd0a00770bee0f9449e8d5dcd810
YDEAX41 <yoshi_degent@hotmail.com>
2015-10-01 16:18:55 +0200

    data binding example

--- app/scripts/directives/customDirective.js
+++ app/scripts/directives/customDirective.js
@@ -6,7 +6,7 @@
         templateUrl: 'views/customDirective.html',
         restrict: 'EA',// E: can be used as element, A: can be used as template
         scope: {
-          myAttr: '@'
+          myAttr: '=' // @: one-way binding, =: two-way binding
         }
       }
     });
